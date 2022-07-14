b1b7d01c15db10d90f4fb2beffe628a7c528c960
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-14 17:57:47 +0530

    Configer Tailwind CSS

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Login(){
+    return (
+        <div>
+
+        <Head>
+            <title>Login</title>
+        </Head>
+
+
+            <h1>Login</h1>
+        </div>
+    )
+}
---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Register(){
+    return (
+        <div>
+
+
+        <Head>
+            <title>Register</title>
+        </Head>
+
+            <h1>Register</h1>
+        </div>
+    )
+}
