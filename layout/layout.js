086b75672a6d6f0611e0af59985cb1ca49861255
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-15 17:37:37 +0530

    Live Image Added

--- layout/layout.js
+++ layout/layout.js
@@ -1,9 +1,15 @@
+
+import styles from '../styles/Layout.module.css';
+
+
 export default function Layout( { children }){
     return (
         <div className="flex h-screen bg-blue-400">
             <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
-                <div>
-                    Images
+                <div className={styles.imgStyle}>
+                    <div className={styles.cartoonImg}></div>
+                    <div className={styles.cloud_one}></div>
+                    <div className={styles.cloud_two}></div>
                 </div>
                 <div className="right flex flex-col justify-evenly bg-gray-500">
                     <div className="text-center py-10">
