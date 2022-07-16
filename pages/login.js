769322198bcfebb9bab76d769049712cca2785df
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-16 17:56:35 +0530

    React Icons Added

--- pages/login.js
+++ pages/login.js
@@ -3,8 +3,13 @@ import Layout from '../layout/layout'
 import Link from 'next/link'
 import styles from '../styles/Form.module.css';
 import Image from 'next/image'
+import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
+import { useState } from 'react';
 
 export default function Login(){
+
+    const [show, setShow] = useState(false)
+
     return (
         <Layout>
 
@@ -27,14 +32,20 @@ export default function Login(){
                     placeholder='Email'
                     className={styles.input_text}
                     />
+                    <span className='icon flex items-center px-4'>
+                        <HiAtSymbol size={25} />
+                    </span>
                 </div>
                 <div className={styles.input_group}>
                     <input 
-                    type="password"
+                    type={`${show ? "text" : "password"}`}
                     name='password'
                     placeholder='password'
                     className={styles.input_text}
                     />
+                     <span className='icon flex items-center px-4' onClick={() => setShow(!show)}>
+                        <HiFingerPrint size={25} />
+                    </span>
                 </div>
 
                 {/* login buttons */}
