9ed2f95e272ecf4302c219773b753c8eb3419c62
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-20 17:43:01 +0530

    Protected Routes Added

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { useState } from 'react'
-import { useSession } from "next-auth/react"
+import { getSession, useSession, signOut } from "next-auth/react"
 
 export default function Home() {
 
   const { data: session } = useSession()
+
+  function handleSignOut(){
+    signOut()
+  }
 
   return (
     <div className={styles.container}>
@@ -15,7 +19,7 @@ export default function Home() {
         <title>Home Page</title>
       </Head>
 
-      {session ? User({ session }) : Guest()}
+      {session ? User({ session, handleSignOut }) : Guest()}
     </div>
   )
 }
@@ -34,7 +38,7 @@ function Guest(){
 }
 
 // Authorize User
-function User({ session }){
+function User({ session, handleSignOut }){
   return(
     <main className="container mx-auto text-center py-20">
           <h3 className='text-4xl font-bold'>Authorize User Homepage</h3>
@@ -45,7 +49,7 @@ function User({ session }){
           </div>
 
           <div className="flex justify-center">
-            <button className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50'>Sign Out</button>
+            <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50'>Sign Out</button>
           </div>
 
           <div className='flex justify-center'>
@@ -53,4 +57,23 @@ function User({ session }){
           </div>
       </main>
   )
+}
+
+
+export async function getServerSideProps({ req }){
+  const session = await getSession({ req })
+
+  if(!session){
+    return {
+      redirect : {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: { session }
+  }
+
 }
