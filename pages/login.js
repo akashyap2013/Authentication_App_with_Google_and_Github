cd58839f881bdbebea99f84ec862720fc2b671ab
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-22 18:05:19 +0530

    Validation Added

--- pages/login.js
+++ pages/login.js
@@ -7,15 +7,18 @@ import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
 import { useState } from 'react';
 import { signIn, signOut } from "next-auth/react"
 import { useFormik } from 'formik';
+import login_validate from '../lib/validate';
 
 export default function Login(){
 
     const [show, setShow] = useState(false)
+    // formik hook
     const formik = useFormik({
         initialValues: {
             email: '',
             password: ''
         },
+        validate : login_validate,
         onSubmit
     })
 
@@ -59,7 +62,10 @@ export default function Login(){
                     <span className='icon flex items-center px-4'>
                         <HiAtSymbol size={25} />
                     </span>
+                   
                 </div>
+                {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>}
+
                 <div className={styles.input_group}>
                     <input 
                     type={`${show ? "text" : "password"}`}
@@ -71,8 +77,10 @@ export default function Login(){
                      <span className='icon flex items-center px-4' onClick={() => setShow(!show)}>
                         <HiFingerPrint size={25} />
                     </span>
+                   
                 </div>
 
+                {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>}
                 {/* login buttons */}
                 <div className="input-button">
                     <button type='submit' className={styles.button}>
--- pages/register.js
+++ pages/register.js
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from "react-icons/hi";
 import { useState } from 'react';
 import { useFormik } from 'formik';
+import { registerValidate } from '../lib/validate'
 
 export default function Register(){
 
@@ -17,6 +18,7 @@ export default function Register(){
             password: '',
             cpassword: ''
         },
+        validate: registerValidate,
         onSubmit
     })
 
@@ -52,6 +54,7 @@ export default function Register(){
                         <HiOutlineUser size={25} />
                     </span>
                 </div>
+                {formik.errors.username && formik.touched.username ? <span className='text-rose-500'>{formik.errors.username}</span> : <></>}
                 <div className={styles.input_group}>
                     <input 
                     type="email"
@@ -64,6 +67,7 @@ export default function Register(){
                         <HiAtSymbol size={25} />
                     </span>
                 </div>
+                {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>}
                 <div className={styles.input_group}>
                     <input 
                     type={`${show.password ? "text" : "password"}`}
@@ -76,6 +80,7 @@ export default function Register(){
                         <HiFingerPrint size={25} />
                     </span>
                 </div>
+                {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>}
 
                 <div className={styles.input_group}>
                     <input 
@@ -89,6 +94,7 @@ export default function Register(){
                         <HiFingerPrint size={25} />
                     </span>
                 </div>
+                {formik.errors.cpassword && formik.touched.cpassword ? <span className='text-rose-500'>{formik.errors.cpassword}</span> : <></>}
 
                 {/* login buttons */}
                 <div className="input-button">
