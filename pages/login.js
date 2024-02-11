47b947d203eab2ff502c226475ebbe5740627474
sanjith <119114432+sanjithganesa@users.noreply.github.com>
2024-02-11 08:11:57 +0000

    this is my commit1

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Layout from "../layout/layout";
+
+export default function Login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login</h1>
+    </Layout>
+  );
+}
